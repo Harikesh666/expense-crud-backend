c73c949a8ad985dda9383f59fa73f665e7768dc6
Harikesh Mishra <mharikesh11@gmail.com>
2025-08-11 18:52:19 +0530

    Updated index.js
    added deployed frontend url in cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://expense-crud-frontend.vercel.app/" }));
 
 pool.connect()
     .then((client) => {
